5453e6a121b1be11079a42ca8685d1a02e6f9935
Isabelle Ferreira <isabelle.a.ferreira@gmail.com>
2020-03-27 17:37:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ function add (a,b) {
   number = a + b;
   return number;
 }
+
+function subtract (a,b) {
+  number = a - b;
+  return number;
+}
+
+function multiple (a,b) {
+  number = a * b;
+  return number;
+}
+
+function divide (a,b) {
+  number = a / b;
+  return number;
+}
